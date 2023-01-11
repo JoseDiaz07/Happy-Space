79b68106b32616b53841cabb0f6b2967545a2710
Jose <diazmarinjoseelias2007@gmail.com>
2023-01-11 16:52:04 -0500

    Upgrade

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,35 +4,33 @@ const Home = () => {
   const [catImg, setCatImg] = useState("")
   const [dogImg, setDogImg] = useState("")
 
-  
+  //* Traer las imagenes 
+  const getCat = async () => {
+    try {
+      const res = await fetch('https://aws.random.cat/meow')
+      const data = await res.json()
+      setCatImg(data.file)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  const getDog = async () => {
+    try {
+      const res = await fetch('https://dog.ceo/api/breeds/image/random')
+      const data = await res.json()
+      setDogImg(data.message)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   //* Iniciar la carga de imagenes
   useEffect(() => {
-    fetch('https://aws.random.cat/meow')
-      .then(res => res.json())
-      .then(data => setCatImg(data.file))
-      .catch(console.error("Something bad happened"))
-
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(res => res.json())
-      .then(data => setDogImg(data.message))
-      .catch(console.error("Something bad happened"))
-
+    getCat()
+    getDog()
   }, [])
-
-  //* Traer las imagenes 
-  const getCat = () => {
-    fetch('https://aws.random.cat/meow')
-      .then(res => res.json())
-      .then(data => setCatImg(data.file))
-      .catch(console.error("Something bad happened"))
-  }
-
-  const getDog = () => {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(res => res.json())
-      .then(data => setDogImg(data.message))
-      .catch(console.error("Something bad happened"))
-  }
 
   return (
     <div className="min-h-screen bg-slate-100 dark:bg-[#121212] dark:text-white transition-all duration-500">
@@ -51,7 +49,7 @@ const Home = () => {
         <div className="w-72 shadow-lg p-5 rounded-md text-center bg-slate-100 dark:bg-[#28292a] mx-auto mt-10 md:mt-0 md:mx-0 transition-all duration-500">
           <div className="rounded-md w-60 h-64 bo">
             <img src={dogImg} alt="" className="rounded-md w-60 h-64 object-cover" />
-          </div>    
+          </div>
           <div className="flex gap-x-5 duration-75">
             <button onClick={getDog} className="py-2 px-5 mt-5 outline outline-1 rounded-md hover:bg-black hover:text-slate-100 dark:hover:bg-white dark:hover:text-black active:bg-none ">Get a dog</button>
             <a href={dogImg} download target="_blank" rel="noreferrer" className="py-2 px-5 mt-5 outline outline-1 rounded-md hover:bg-black hover:text-slate-100 dark:hover:bg-white dark:hover:text-black active:bg-none">Download</a>
